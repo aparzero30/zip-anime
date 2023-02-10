e69fadc2f5211152b486d721202c22b826eb1ec4
aparzero <jimcin30@gmail.com>
2023-02-10 21:52:10 +0800

    first commit

---
+++ src/main/java/com/example/zipanime/ZipAnimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.zipanime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipAnimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipAnimeApplication.class, args);
+	}
+
+}
