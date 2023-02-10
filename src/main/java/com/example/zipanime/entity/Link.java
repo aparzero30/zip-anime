a17f21bf1b523ae1697f5d3c46e48999df43b34e
aparzero <jimcin30@gmail.com>
2023-02-10 23:06:21 +0800

    Added controller

--- src/main/java/com/example/zipanime/entity/Link.java
+++ src/main/java/com/example/zipanime/entity/Link.java
@@ -1,5 +1,9 @@
 package com.example.zipanime.entity;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Link {
     private String self;
     private String related;
